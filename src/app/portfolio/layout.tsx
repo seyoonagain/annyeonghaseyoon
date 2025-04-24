a6ed24391ec69af512908817ea05c2a14193e0f0
Seyoon Jeong <seyoonagain@gmail.com>
2025-04-24 15:02:37 +0900

    chore: adjust portfolio layout

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,11 +1,7 @@
 import { Container } from '@/components/layout';
 
 const Layout = ({ children }) => {
-  return (
-    <Container className="flex flex-col px-0">
-      <div>{children}</div>
-    </Container>
-  );
+  return <Container>{children}</Container>;
 };
 
 export default Layout;
