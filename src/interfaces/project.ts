ad05ed2f6d838ddb6deea795887963ad08e5a853
Seyoon Jeong <seyoonagain@gmail.com>
2025-04-26 02:51:20 +0900

    feat: define types and create API for post and project data

---
+++ src/interfaces/project.ts
@@ -0,0 +1,11 @@
+export type Project = {
+  slug: string;
+  team: string;
+  description: string;
+  startedAt: string;
+  endedAt: string;
+  techStack: string[];
+  github: string;
+  demo: string;
+  content: string;
+};
