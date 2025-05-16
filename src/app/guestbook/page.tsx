d83d2d9b09013101a665a4ab193139ed4c68e5bb
Seyoon Jeong <seyoonagain@gmail.com>
2025-05-17 04:38:04 +0900

    feat: add animated greeting to guestbook page

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,8 +1,33 @@
+'use client';
+
 import { Comment } from '@/components/common';
+import { motion } from 'framer-motion';
 
 const Guestbook = () => {
   return (
-    <div className="w-full max-w-5xl">
+    <div className="flex flex-col items-center gap-12 w-full max-w-5xl z-50">
+      <div className="flex flex-col items-start gap-4 relative">
+        <h2 className="content-center font-light text-5xl sm:text-6xl md:text-7xl tracking-tighter">
+          Seyoonagain{'\xa0\xa0'}
+          <motion.span
+            animate={{ rotate: [0, 15, -10, 10, -7, 7, -2, 0] }}
+            transition={{
+              duration: 2.7,
+              repeat: Infinity,
+              repeatDelay: 1.3,
+              ease: 'easeInOut',
+            }}
+            className="inline-block"
+          >
+            👋🏼
+          </motion.span>
+        </h2>
+        <p className="text-sm leading-5 tracking-tight">
+          방문해주셔서 감사합니다.
+          <br />
+          흔적을 남겨주세요!
+        </p>
+      </div>
       <Comment loading="eager" reactionsEnabled="0" />
     </div>
   );
